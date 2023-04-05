d0a36f1641cb7c7d66e2f0181fbe663ff7c9c0ed
Jordan Irving <110358946+JordanIrving1@users.noreply.github.com>
2023-04-05 17:16:35 +0100

    Decrypting files

--- Minix.c
+++ Minix.c
@@ -44,3 +44,39 @@ int encrypt_file(const char* input_filename, const char* output_filename, RSA* k
 
     return 0;
 
+// Decrypt a file using RSA
+int decrypt_file(const char* input_filename, const char* output_filename, RSA* key) {
+    FILE* input_file = fopen(input_filename, "rb");
+    if (input_file == NULL) {
+        printf("Failed to open input file %s\n", input_filename);
+        return -1;
+    }
+
+    FILE* output_file = fopen(output_filename, "wb");
+    if (output_file == NULL) {
+        printf("Failed to open output file %s\n", output_filename);
+        fclose(input_file);
+        return -1;
+    }
+
+    unsigned char input_block[KEY_LENGTH / 8];
+    unsigned char output_block[BLOCK_SIZE];
+
+    int bytes_read = 0;
+    while ((bytes_read = fread(input_block, 1, KEY_LENGTH / 8, input_file)) > 0) {
+        int decrypted_size = RSA_private_decrypt(bytes_read, input_block, output_block, key, RSA_PKCS1_PADDING);
+        if (decrypted_size == -1) {
+            printf("Failed to decrypt input block\n");
+            fclose(input_file);
+            fclose(output_file);
+            return -1;
+        }
+
+        fwrite(output_block, 1, decrypted_size, output_file);
+    }
+
+    fclose(input_file);
+    fclose(output_file);
+
+    return 0;
+}
